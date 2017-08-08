a5c4632302fe3871fe25b295bd7a9022602012c8
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-08 10:01:00 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(collection)
+  i = 0
+  newA = Array.new
+  while i < collection.size
+    x = collection[i]
+    yield(x)
+    newA << yield(x)
+    i += 1
+  end
+  return newA
+end
